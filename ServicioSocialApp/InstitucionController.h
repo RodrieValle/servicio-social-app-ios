5c3c2e88075b5c2fa469d5a1ead9530bfda22c58
Danie Pérez <pdm-115@PDM-115s-Mac.local>
2014-05-20 13:06:00 -0600

    Institución

---
+++ ServicioSocialApp/InstitucionController.h
@@ -0,0 +1,13 @@
+//
+//  InstitucionController.h
+//  ServicioSocialApp
+//
+//  Created by PDM-115 on 20/05/14.
+//  Copyright (c) 2014 Rodrigo Herrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstitucionController : UIViewController
+
+@end
---
+++ ServicioSocialApp/InstitucionController.m
@@ -0,0 +1,38 @@
+//
+//  InstitucionController.m
+//  ServicioSocialApp
+//
+//  Created by PDM-115 on 20/05/14.
+//  Copyright (c) 2014 Rodrigo Herrera. All rights reserved.
+//
+
+#import "InstitucionController.h"
+
+@interface InstitucionController ()
+
+@end
+
+@implementation InstitucionController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
