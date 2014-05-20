23a27b1947e985c36325e69acb045b704c5c4243
PDM-115 <pdm-115@PDM-115s-Mac.local>
2014-05-20 12:25:16 -0600

    Alumno

---
+++ AlumnoViewController.m
@@ -0,0 +1,38 @@
+//
+//  AlumnoViewController.m
+//  ServicioSocialApp
+//
+//  Created by PDM-115 on 20/05/14.
+//  Copyright (c) 2014 Rodrigo Herrera. All rights reserved.
+//
+
+#import "AlumnoViewController.h"
+
+@interface AlumnoViewController ()
+
+@end
+
+@implementation AlumnoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
